b4e1d05c376b15c6de5b329cd4ef70f3468c6b9a
akashshevgan <akashshevgan11@gmail.com>
2021-07-03 12:18:46 +0530

    adding Dictionaries operations

--- data_structure.py
+++ data_structure.py
@@ -5,3 +5,10 @@ fruits.count('apple')
 
 basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
 print(basket)
+
+'orange' in basket
+
+# Dictionaries operations
+
+tel = {'jack': 4098, 'sape': 4139}
+print (tel)
