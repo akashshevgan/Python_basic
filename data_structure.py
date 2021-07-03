3c5eda028ccca348d5deae064877eb3e08ecbbdf
akashshevgan <akashshevgan11@gmail.com>
2021-07-03 12:13:53 +0530

    adding set operations

--- data_structure.py
+++ data_structure.py
@@ -1,2 +1,7 @@
 fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 fruits.count('apple')
+
+# set operations
+
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket)
